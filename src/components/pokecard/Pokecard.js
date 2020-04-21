c3a114a448bc7fe33264d33eca30f7ca05dd1e6c
ellfiscina <ellfiscina@gmail.com>
2020-04-21 15:32:50 +0200

    add list of pokemons

--- src/components/pokecard/Pokecard.js
+++ src/components/pokecard/Pokecard.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import './Pokecard.css';
 const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-
 class Pokecard extends Component {
     render() {
-        const { id, name, type, exp } = this.props;
+        const { id, name, type, base_experience } = this.props.attr;
         let imgSrc = `${POKE_API}${id}.png`;
         return (
             <div className="Pokecard">
                 <h1>{name}</h1>
                 <img src={imgSrc} alt={name} />
                 <p>Type: {type}</p>
-                <p>Exp: {exp}</p>
+                <p>Exp: {base_experience}</p>
             </div>
         );
     }
--- src/components/pokedex/Pokedex.js
+++ src/components/pokedex/Pokedex.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import Pokecard from '../pokecard/Pokecard';
+import {pokemonList} from '../../helpers';
 import './Pokedex.css';
 
 class Pokedex extends Component {
     render() {
         return(
             <div className="Pokedex">
-                <Pokecard id="4" name="Charmander" type="fire" exp="62"/>
+                <ul>
+                    {pokemonList.map(i => <li key={i.id}><Pokecard attr={i}/></li>)}
+                </ul>
             </div>
         );
     }
