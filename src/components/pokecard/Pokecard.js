c7f2ff808cccff92b9fcb7c42dade791a332dc00
ellfiscina <ellfiscina@gmail.com>
2020-04-21 15:37:18 +0200

    change img source

--- src/components/pokecard/Pokecard.js
+++ src/components/pokecard/Pokecard.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
+function formatId(id) {
+    return ('000' + id).slice(-3);
+}
 class Pokecard extends Component {
     render() {
         const { id, name, type, base_experience } = this.props.attr;
-        let imgSrc = `${POKE_API}${id}.png`;
+        let imgSrc = `${POKE_API}${formatId(id)}.png`;
+
         return (
             <div className="Pokecard">
                 <h1>{name}</h1>
