1293e38eddad98c5bfdac5bd71f0953330df2423
ellfiscina <ellfiscina@gmail.com>
2020-06-05 12:00:58 +0200

    make it pretty

--- src/components/pokecard/Pokecard.js
+++ src/components/pokecard/Pokecard.js
@@ -8,12 +8,16 @@ function formatId(id) {
 class Pokecard extends Component {
     render() {
         const { id, name, type, base_experience } = this.props.attr;
-        let imgSrc = `${POKE_API}${formatId(id)}.png`;
+        const formatted_id = formatId(id);
+        let imgSrc = `${POKE_API}${formatted_id}.png`;
 
         return (
             <div className={"Pokecard " + type}>
-                <div className="Pokecard-media">
+                <div className="Pokecard-header">
+                    <p className="Pokecard-id">#{formatted_id}</p>
                     <p className="Pokecard-exp">{base_experience} XP</p>
+                </div>
+                <div className="Pokecard-media">
                     <img src={imgSrc} alt={name} />
                 </div>
                 <hr />
